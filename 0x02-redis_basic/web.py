618ce6e83895bad479a2e92e23f020b647d7d277
elbatouri rabi <elbatouri@gmail.Com>
2024-03-28 01:42:05 +0000

    push advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+'''A module for fetching web page content with caching and tracking.'''
+
 import redis
 import requests
-from functools import wraps
-from typing import Callable
-
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
 
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+def data_cacher(method):
+    '''Decorator to cache fetched data and track requests.'''
+    def invoker(url):
+        '''Wrapper function for caching fetched data.'''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.set(f'result:{url}', result, ex=10)
         return result
     return invoker
 
-
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    '''Returns URL content, caching response and tracking request.'''
     return requests.get(url).text
+
+if __name__ == "__main__":
+    # Test the get_page function
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
+    print(get_page(url))  # This should return the HTML content of the example.com page
