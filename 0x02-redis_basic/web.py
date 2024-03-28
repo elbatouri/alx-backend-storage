0ca0f05c8a0da81742f2df6a32127464e14c44f0
elbatouri rabi <elbatouri@gmail.Com>
2024-03-28 01:47:23 +0000

    push the corrected code pass pycodestyle

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
-'''A module for fetching web page content with caching and tracking.'''
+'''Module for fetching web page content with caching and tracking.'''
 
 import redis
 import requests
 
 redis_store = redis.Redis()
 
+
 def data_cacher(method):
     '''Decorator to cache fetched data and track requests.'''
     def invoker(url):
         '''Wrapper function for caching fetched data.'''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
+        key_count = f'count:{url}'
+        key_result = f'result:{url}'
+        redis_store.incr(key_count)
+        result = redis_store.get(key_result)
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'result:{url}', result, ex=10)
+        redis_store.set(key_result, result, ex=10)
         return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
     '''Returns URL content, caching response and tracking request.'''
     return requests.get(url).text
 
+
 if __name__ == "__main__":
     # Test the get_page function
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
-    print(get_page(url))  # This should return the HTML content of the example.com page
+    url = (
+        "http://slowwly.robertomurray.co.uk/delay/1000/"
+        "url/https://www.example.com"
+    )
+    print(get_page(url))
