9ffaa7f4199b84b79d812c227acb0ab274129b17
elbatouri rabi <elbatouri@gmail.Com>
2024-03-28 01:40:36 +0000

    push advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-'''Module for request caching and tracking using Redis.'''
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
-'''Module-level Redis instance for caching and tracking.'''
+'''The module-level Redis instance.
+'''
+
 
 def data_cacher(method: Callable) -> Callable:
-    '''Decorator to cache fetched data and track requests.'''
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
     def invoker(url) -> str:
-        '''Wrapper function for caching fetched data.'''
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -23,7 +29,10 @@ def data_cacher(method: Callable) -> Callable:
         return result
     return invoker
 
+
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns URL content, caching response and tracking request.'''
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
