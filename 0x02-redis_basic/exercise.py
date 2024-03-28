3d28075f5d0c34f65a039d6e7ecee6efad9e2e4b
elbatouri rabi <elbatouri@gmail.Com>
2024-03-28 01:34:21 +0000

    push task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
-'''using redis nosql storage'''
+'''Using redis nosql storage'''
+
 import uuid
 import redis
+from functools import wraps
 from typing import Callable, Union
 
 
+def count_calls(method: Callable) -> Callable:
+    '''Decorator to count how many times methods of the Cache class are called.'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''Wrapper function that increments the count and calls the original method.'''
+        # Increment count
+        self._redis.incr(method.__qualname__)
+        # Call the original method
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
-    '''Represents an object for storing data in a Redis data storage.
-    '''
+    '''Represents an object for storing data in a Redis data storage.'''
     def __init__(self) -> None:
-        '''Initializes a Cache instance and connects to Redis.
-        '''
+        '''Initializes a Cache instance and connects to Redis.'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Stores a value in Redis and returns the key.
-        '''
+        '''Stores a value in Redis and returns the key.'''
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, None]:
-        '''Retrieves a value from Redis and optionally applies a conversion function.
-        '''
-        value = self._redis.get(key)
-        if value is not None and fn is not None:
-            return fn(value)
-        return value
+    def get(self, key: str) -> Union[str, bytes, None]:
+        '''Retrieves a value from Redis.'''
+        return self._redis.get(key)
 
     def get_str(self, key: str) -> str:
-        '''Retrieves a string value from Redis.
-        '''
-        return self.get(key, fn=lambda x: x.decode('utf-8'))
+        '''Retrieves a string value from Redis.'''
+        return self.get(key).decode('utf-8')
 
     def get_int(self, key: str) -> int:
-        '''Retrieves an integer value from Redis.
-        '''
-        return self.get(key, fn=lambda x: int(x))
+        '''Retrieves an integer value from Redis.'''
+        return int(self.get(key))
